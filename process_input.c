fd4c46a02746f81e3bd9a2c98e5e08e86febaf84
Ilan OHAYON <iohayon@e3r3p16.42.fr>
2019-02-16 19:11:04 +0100

    2

--- process_input.c
+++ process_input.c
@@ -6,7 +6,7 @@
 /*   By: iohayon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/03 16:14:41 by iohayon           #+#    #+#             */
-/*   Updated: 2019/02/03 16:25:11 by iohayon          ###   ########.fr       */
+/*   Updated: 2019/02/16 19:10:43 by iohayon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,18 @@ int		process_input()
 	i = 0;
 	while (get_next_line(fd, &line))
 	{
-		if (ft_strchr(line[i]) != "#" || ft_strchr(line[i]) != ".")
-			return (0);
+		if (i % 4 != 0 || i == 0)
+		{
+			if (ft_strchr(line[i]) != "#" || ft_strchr(line[i]) != ".")
+				return (0);
+			if (ft_strlen(line[i]) != 4)
+				return (0);
+		}
+		if (i & 4 == 0)
+			if (ft_strchr(line[i]) != '\n')
+				return (0);
+		i++;
 	}
+	if (i != 20)
+		return (0);
 }
